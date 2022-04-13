dc1f17132b6313fc949bc6f5ac6067c2be61774d
Guilherme Colares <guicolares@gmail.com>
2022-04-13 11:10:52 -0300

    structure

---
+++ Shared/pocbitriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  pocbitriseApp.swift
+//  Shared
+//
+//  Created by Guilherme Leite Colares on 13/04/22.
+//
+
+import SwiftUI
+
+@main
+struct pocbitriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
